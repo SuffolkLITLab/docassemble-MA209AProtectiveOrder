170a2f0500bdcdff1f23c461ba68395deaf7bd98
Quinten Steenhuis <qsteenhuis@suffolk.edu>
2023-09-26 12:11:42 -0400

    Bump version and dependency

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MA209AProtectiveOrder',
-      version='2.1.1',
+      version='2.1.2',
       description=(''),
       long_description='# docassemble.MA209AProtectiveOrder\r\n\r\nGuided interview that helps a domestic violence survivor complete forms related to restraining order. Including custody, impoundment, etc.\r\n\r\n\r\n\r\n## Author\r\n\r\nQuinten Steenhuis\r\nCaroline Robinson\r\nKate Barry\r\nPlocket\r\nLily Yang\r\nMatthew Brooks\r\nLance Godard\r\nMaeve MacGlinchey\r\nKendall Garner\r\nDavid Colarusso\r\n',
       long_description_content_type='text/markdown',
@@ -54,7 +54,7 @@ setup(name='docassemble.MA209AProtectiveOrder',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALToolbox>=0.8.2', 'docassemble.AssemblyLine>=2.23.0', 'docassemble.MACourts>=0.59.1', 'docassemble.MassAccess>=0.3.0'],
+      install_requires=['docassemble.ALToolbox>=0.8.2', 'docassemble.AssemblyLine>=2.26.0', 'docassemble.MACourts>=0.59.1', 'docassemble.MassAccess>=0.3.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/MA209AProtectiveOrder/', package='docassemble.MA209AProtectiveOrder'),
      )
